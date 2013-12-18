cc8932b378eae85d0cc92572687e4a61d3893a17
whitedot <whitedot@naver.com>
2013-12-18 15:51:54 +0900

    결과 없는 경우 안내 문구 마크업 수정

--- api/search.php
+++ api/search.php
@@ -122,7 +122,7 @@ if ($res === false) {
     if($i > 0)
         echo '</ul>';
     else
-        echo '<div class="result_msg">검색결과가 없습니다.</div>';
+        echo '<div class="result_msg result_fail">검색결과가 없습니다.</div>';
 
     //echo '<p>실행시간 : '.$res['time'].'</p>';
 
